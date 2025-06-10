41bd734fd53cd643534e32f478bb9354676303a8
J-B-Hub <juan.ballesteros30224@ucaldas.edu.co>
2025-06-09 21:24:28 -0500

    password corregida

--- front-con-seguridad-main/src/app/services/password.service.ts
+++ front-con-seguridad-main/src/app/services/password.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Password } from '../models/password.model';
 import { environment } from 'src/environments/environment';
+import { Password } from '../models/password.model';
 
 @Injectable({
   providedIn: 'root'
